d0a150bbd30fd91603fb7bb5d4086b1fe2a97782
Vinith Krishnan <vinithk@gmail.com>
2020-11-26 14:44:17 -0600

    finished curve.go impl

--- crypto/ed25519/curve.go
+++ crypto/ed25519/curve.go
@@ -1,4 +1,4 @@
-package ed25519
+package main
 
 
 /*
@@ -11,7 +11,7 @@ package ed25519
 */
 import "C"
 import (
-	// "fmt"
+	"fmt"
 	// "math"
 	"math/big"
 	"errors"
@@ -20,6 +20,7 @@ import (
 	"unsafe"
 	"crypto/sha512"
 	"reflect"
+	"crypto/rand"
 )
 // libsodium linking code above import C statement
 // type ldconfig if lib sodium is not found
@@ -141,29 +142,29 @@ func (p Point) Not_equal(o Point) (bool) { //TODO test function
 	return !(C.sodium_memcmp(unsafe.Pointer(&p.val[0]), unsafe.Pointer(&o.val[0]) , 32) == 0)
 }
 
-func (p Point) Add(o Point) (Point,error) { //TODO test function
+func (p Point) Add(o Point) (Point) { // removed error handling
 	result:= Raw_point()
 	if C.crypto_core_ed25519_add((*C.uchar)(&result.val[0]), (*C.uchar)(&p.val[0]), (*C.uchar)(&o.val[0])) == 0 {
-		return result,nil
+		return result
 	}
-	return result,errors.New("add ain't working")
+	return result
 }
 
 
-func (p Point) Sub(o Point) (Point,error) { //TODO test function
+func (p Point) Sub(o Point) (Point) { //removed error handling
 	result:= Raw_point()
 	if C.crypto_core_ed25519_sub((*C.uchar)(&result.val[0]), (*C.uchar)(&p.val[0]), (*C.uchar)(&o.val[0])) == 0 {
-		return result,nil
+		return result
 	}
-	return result,errors.New("sub ain't working")
+	return result
 }
 
-func (p Point) Mul(s Scalar) (Point,error) { //TODO test function
+func (p Point) Mul(s Scalar) (Point) {  // removed error handling
 	temp := Raw_point()
 	if C.crypto_scalarmult_ed25519_noclamp((*C.uchar)(&temp.val[0]),(*C.uchar)(&s.val[0]),(*C.uchar)(&p.val[0])) == 0 {
-		return temp,nil
+		return temp
 	}
-	return temp,errors.New("Scalarmult_ed25519_noclamp not working")
+	return temp
 }
 
 
@@ -212,24 +213,42 @@ func Raw_scalar() (Scalar) {
 }
 // returns scalar from little endian rep of value
 func Scalar_from_bytes(data []byte) (Scalar,error) {
+	copy_data := make([]byte,len(data))
 	if len(data)!= 32 {
 		return Raw_scalar(),errors.New("len data must be 32")
 	}
+	copy(copy_data,data)
 	sc:= Raw_scalar()
-	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
-		data[i], data[j] = data[j], data[i]
+	for i, j := 0, len(copy_data)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
+		copy_data[i], copy_data[j] = copy_data[j], copy_data[i]
 	}
-	sc.bint = *new(big.Int).SetBytes(data)
+	sc.bint = *new(big.Int).SetBytes(copy_data)
 	sc.val = data
 	return sc,nil
 
 }
+// set/refresh value of bint from val
+func (sc Scalar) Refresh_bint() {
+	copy_data := make([]byte,len(sc.val))
+	if len(sc.val)!= 32 {
+	// print error
+	}
+	copy(copy_data,sc.val)
+	for i, j := 0, len(copy_data)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
+		copy_data[i], copy_data[j] = copy_data[j], copy_data[i]
+	}
+	sc.bint = *new(big.Int).SetBytes(copy_data)
+}
 
-func Scalar_reduce(data []byte) (Scalar) { //IMPLEMENT
+func Scalar_reduce(data []byte) (Scalar) { 
 	// obtain a uniformly distributed scalar value from a at least 40 bytes (~317 bit) random data,
            // typically the output of a cryptographic hashfunction
-	
-	return Raw_scalar()
+	scalar:=Raw_scalar()
+	if len(data)>=40 {
+		C.crypto_core_ed25519_scalar_reduce((*C.uchar)(&scalar.val[0]),(*C.uchar)(&data[0]))
+	}
+	scalar.Refresh_bint()
+	return scalar
 }
 
 
@@ -241,66 +260,90 @@ func (s Scalar) Is_valid() (bool) {  // TODO test this fun
 	return false
 }
 
-func Random() (Scalar) { // IMPLEMENT
- return Raw_scalar()
+func Random() (Scalar) {  //TODO: make it seedable
+ rand_big_int,_ := rand.Int(rand.Reader,GROUP_ORDER)
+ return New_scalar(*rand_big_int)
 }
 
 func (s Scalar) Equal(o Scalar) (bool){ // Test this function
-	if reflect.DeepEqual(s,o) {
-		return true
-	}
+	// if reflect.DeepEqual(s,o) {
+	// 	return true
+	// }
 	return C.sodium_memcmp(unsafe.Pointer(&s.val[0]), unsafe.Pointer(&o.val[0]) , 32)==0
 }
 
 func (s Scalar) Not_equal(o Scalar) (bool){ // Test this function
-	if reflect.DeepEqual(s,o) {
-		return false
-	}
+	// if reflect.DeepEqual(s,o) {
+	// 	return false
+	// }
 	return C.sodium_memcmp(unsafe.Pointer(&s.val[0]), unsafe.Pointer(&o.val[0]) , 32)!=0
 }
 
-func (a Scalar) Add(b Scalar) (Scalar) { // IMPLEMENT
-  return Raw_scalar()
+func (a Scalar) Add(b Scalar) (Scalar) { 
+  result:= Raw_scalar()
+  C.crypto_core_ed25519_scalar_add((*C.uchar)(&result.val[0]), (*C.uchar)(&a.val[0]), (*C.uchar)(&b.val[0]))
+  result.Refresh_bint()
+  return result
+
 }
 
-func (a Scalar) Sub(b Scalar) (Scalar) { // IMPLEMENT
-	return Raw_scalar()
+
+func (a Scalar) Sub(b Scalar) (Scalar) { 
+	result:= Raw_scalar()
+	C.crypto_core_ed25519_scalar_sub((*C.uchar)(&result.val[0]), (*C.uchar)(&a.val[0]), (*C.uchar)(&b.val[0])) 
+	result.Refresh_bint()
+	return result
 }
 
-func (a Scalar) Mul(b Scalar) (Scalar) { // IMPLEMENT
-	return Raw_scalar()
+func (a Scalar) Mul(b Scalar) (Scalar) { 
+	result:= Raw_scalar()
+	C.crypto_core_ed25519_scalar_mul((*C.uchar)(&result.val[0]), (*C.uchar)(&a.val[0]), (*C.uchar)(&b.val[0]))
+	result.Refresh_bint()
+	return result
 }
 
-func (a Scalar) Div(b Scalar) (Scalar) { // IMPLEMENT
-	return Raw_scalar()
+func (a Scalar) Div(b Scalar) (Scalar) { 
+	inv,_:=b.Inverse()
+	return a.Mul(inv)
 }
 
-func (a Scalar) Neg() (Scalar) { // IMPLEMENT
+func (a Scalar) Neg() (Scalar) { 
 	//compute the negation of the current scalar as new scalar
 	// a + neg = 0 (mod CURVE_ORDER)
-	return Raw_scalar()
+	result:=Raw_scalar()
+	C.crypto_core_ed25519_scalar_negate((*C.uchar)(&result.val[0]),(*C.uchar)(&a.val[0]))
+	result.Refresh_bint()
+	return result
 }
 
 // a^b
-func (a Scalar) Pow(b Scalar) (Scalar) { // IMPLEMENT
-	return Raw_scalar()
+func (a Scalar) Pow(b Scalar) (Scalar) { 
+	a_int,_ := Scalar_from_bytes(a.val)
+	b_int,_ := Scalar_from_bytes(b.val)
+	result_int := new(big.Int).Exp(&a_int.bint,&b_int.bint,nil)
+	return New_scalar(*result_int)
 }
 
-func (a Scalar)Negate() (Scalar) { // IMPLEMENT
+func (a Scalar)Negate() { 
 	//compute the negation of the current scalar inplace
 	// a + neg = 0 (mod CURVE_ORDER) using crypto_core_ed25519_scalar_negate
-	return a
+	C.crypto_core_ed25519_scalar_negate((*C.uchar)(&a.val[0]),(*C.uchar)(&a.val[0]))
+	a.Refresh_bint()
 }
 
-func (a Scalar) Inverse() (Scalar,error) { // IMPLEMENT
+func (a Scalar) Inverse() (Scalar,error) { 
 	// return a new Scalar with is the multiplicate inverse of the current one
-	return Raw_scalar(),nil
+	result:= Raw_scalar()
+	C.crypto_core_ed25519_scalar_invert((*C.uchar)(&result.val[0]),(*C.uchar)(&a.val[0]));
+	result.Refresh_bint()
+	return result,nil
 }
 
 
 // compute the multiplicate inverse of the current scalar inplace
-func (a Scalar) Invert() (Scalar,error){ // IMPLEMENT
-	return a,nil
+func (a Scalar) Invert() { 
+	C.crypto_core_ed25519_scalar_invert((*C.uchar)(&a.val[0]),(*C.uchar)(&a.val[0]));
+	a.Refresh_bint()
 }
 
 func (a Scalar) Bytes() ([] byte) {
@@ -333,11 +376,11 @@ func H(message []byte) (Scalar) {  //TODO test this function
 	has:= sha512.New()
 	has.Write(message)
 	bs := has.Sum(nil)
-	for i, j := 0, len(bs)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
-		bs[i], bs[j] = bs[j], bs[i]
-	}
-	num, _ :=strconv.Atoi(string(bs))
-	result:= new(big.Int).Mod(big.NewInt(int64(num)),GROUP_ORDER)
+	// for i, j := 0, len(bs)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
+	// 	bs[i], bs[j] = bs[j], bs[i]
+	// }
+	num, _ :=Scalar_from_bytes(bs)
+	result:= new(big.Int).Mod(&num.bint,GROUP_ORDER)
 	return New_scalar(*result)
 
 }
@@ -352,11 +395,11 @@ func Load_key(secret_key_seed []byte) (Scalar,Point,[]byte,error){
 	bs := has.Sum(nil)
 	secret_bs:= bs[:32]
 	r_seed:= bs[32:]
-	for i, j := 0, len(secret_bs)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
-		secret_bs[i], secret_bs[j] = secret_bs[j], secret_bs[i]
-	}
-	num, _ :=strconv.Atoi(string(secret_bs))
-	a:= big.NewInt(int64(num))
+	// for i, j := 0, len(secret_bs)-1; i < j; i, j = i+1, j-1 { // reversal of bytes
+	// 	secret_bs[i], secret_bs[j] = secret_bs[j], secret_bs[i]
+	// }
+	scl, _ :=Scalar_from_bytes(secret_bs)
+	a := &scl.bint
 	a = new(big.Int).And(a,new(big.Int).Sub(new(big.Int).Exp(big.NewInt(2), big.NewInt(254), nil),big.NewInt(8))) //a &= (1 << 254) - 8
 	a = new(big.Int).Or(a,new(big.Int).Exp(big.NewInt(2), big.NewInt(254), nil)) // a |= 1 << 254
 	secret_key:= New_scalar(*new(big.Int).Mod(a,GROUP_ORDER))
@@ -368,21 +411,23 @@ func Load_key(secret_key_seed []byte) (Scalar,Point,[]byte,error){
 
 }
 
-// func Sign(secret_key_seed []byte,message []byte) (Point,big.Int,error) {
-// 	secret_key, public_key, r_seed , err := load_key(secret_key_seed)
-// 	if err != nil {
-// 		return Raw_point(),*new(big.Int),errors.New("load_Key returned an error")
-// 	}
-// 	r:= H(append(r_seed[:],message[:]...))
-// 	R,_ := Base_times(r)  //TODO: Handle error
-// 	h:= H(append(R.bytes()[:],append(public_key.bytes()[:],message[:]...)...))
-//    // TODO finish scaalr class for this step
-//    return R,
+func Sign(secret_key_seed []byte,message []byte) (Point,Scalar,error) {
+	secret_key, public_key, r_seed , err := Load_key(secret_key_seed)
+	if err != nil {
+		return Raw_point(),Raw_scalar(),errors.New("load_Key returned an error")
+	}
+	r:= H(append(r_seed[:],message[:]...))
+	R,_ := Base_times(r)  //TODO: Handle error
+	h:= H(append(R.Bytes()[:],append(public_key.Bytes()[:],message[:]...)...))
+    s:= r.Add(h.Mul(secret_key))
+   return R,s,nil
+}
+
+// func Sign(secret_key_seed []byte,message []byte) (Point,big.Int,error) { // IMPLEMENT
+//   return Raw_point(),*new(big.Int),nil
 // }
 
-func Sign(secret_key_seed []byte,message []byte) (Point,big.Int,error) { // IMPLEMENT
-  return Raw_point(),*new(big.Int),nil
-}
+
 
 // --------------------------------------------------------------
 type KeyPair struct {
@@ -407,51 +452,92 @@ type KeyPair struct {
     public_key Point
 }
 
-func (kp KeyPair) Init(seed []byte) { // IMPLEMENT
+func (kp KeyPair) Init(seed []byte) { 
+
+	kp.public_key = Raw_point()
+	barray := make([]byte,64)
+	kp.secret_key = SecretKey{barray}
+	if C.crypto_sign_seed_keypair((*C.uchar)(&kp.public_key.val[0]),(*C.uchar)(&kp.secret_key.val[0]),(*C.uchar)(&seed[0])) !=0 {
+		panic("crypto_sign_seed_keypair not working")
+	}
+	kp.seed = kp.secret_key.val[:32]
+	has:= sha512.New()
+	has.Write(kp.seed)
+	bs := has.Sum(nil)
+	sb,_ := Scalar_from_bytes(bs[:32])
+	s:= &sb.bint
+	s = new(big.Int).And(s,new(big.Int).Sub(new(big.Int).Exp(big.NewInt(2), big.NewInt(254), nil),big.NewInt(8))) //s &= (1 << 254) - 8
+	s = new(big.Int).Or(s,new(big.Int).Exp(big.NewInt(2), big.NewInt(254), nil)) // s |= 1 << 254
+	kp.secret_scalar = New_scalar(*new(big.Int).Mod(s,GROUP_ORDER))
+
 
 }
+
 
 func (kp KeyPair) Equal(o KeyPair) (bool){ // TODO test
 	return reflect.DeepEqual(kp.seed,o.seed)
+	
 }
 
-func Random_kp() (KeyPair){
-	return KeyPair{}
+func Random_kp() (KeyPair) {
+	token:= make([]byte,32)
+	rand.Read(token)
+	kp:= KeyPair{}
+	kp.Init(token)
+	return kp
 }
 
 // -----------------------------
 
-func Sign_detached(message []byte,secret_key SecretKey) ([] byte) { // IMPLEMENT
-	dummy:= make([]byte,64)
-	return dummy
+func Sign_detached(message []byte,secret_key SecretKey) ([] byte) { 
+	sig:= make([]byte,64)
+	temp:= C.ulonglong(0)
+	if C.crypto_sign_detached((*C.uchar)(&sig[0]),&temp,(*C.uchar)(&message[0]),C.ulonglong(len(message)),(*C.uchar)(&secret_key.val[0])) !=0 {
+		panic(" sign detached not working")
+	}
+	return sig
 }
 
-func Verify_attached(signed_message []byte,public_key Point) (bool) { // IMPLEMENT
-	return false
+func Verify_attached(signed_message []byte,public_key Point) (bool) {
+	msg_len := len(signed_message) - 64
+	return C.crypto_sign_verify_detached((*C.uchar)(&signed_message[msg_len]),(*C.uchar)(&signed_message[0]),C.ulonglong(msg_len),(*C.uchar)(&public_key.val[0])) == 0
 }
 
 func Verify_detached(message []byte,signature []byte,public_key Point) (bool) { // IMPLEMENT
-	return false
+	return C.crypto_sign_verify_detached((*C.uchar)(&signature[0]),(*C.uchar)(&message[0]),C.ulonglong(len(message)),(*C.uchar)(&public_key.val[0])) == 0
 }
 
 // Testing functionality (will be removed later)
-// func main(){
+func main(){
 
-// 	u := new(big.Int)
-// 	d := new(big.Int)
-// 	u, _ = u.SetString("15112221349535400772501151409588531511454012693041857206046113283949847762202",10)
-// 	d, _ = d.SetString("46316835694926478169428394003475163141307993866256225615783033603165251855960",10)
-// 	pt:= New_point(*u,*d)
-// 	fmt.Println(pt.val)
-// 	fmt.Println(pt.Sign())
-// 	// fmt.Println(C.crypto_core_ed25519_Is_valid_point((*C.uchar)(&pt.val[0])))
-// 	fmt.Println(pt.Is_valid())
-// 	s := New_scalar(*big.NewInt(0))
-// 	fmt.Println(s.val)
-// 	token := make([]byte,32)
-// 	// fmt.Println(C.crypto_scalarmult_ed25519_base_noclamp((*C.uchar)(&token[0]),(*C.uchar)(&s.val[0])))
-// 	C.crypto_scalarmult_ed25519_noclamp((*C.uchar)(&token[0]),(*C.uchar)(&s.val[0]),(*C.uchar)(&pt.val[0]));
-// 	fmt.Println(token)
-// 	fmt.Println(C.crypto_core_ed25519_is_valid_point((*C.uchar)(&token[0])))
+	u := new(big.Int)
+	d := new(big.Int)
+	u, _ = u.SetString("15112221349535400772501151409588531511454012693041857206046113283949847762202",10)
+	d, _ = d.SetString("46316835694926478169428394003475163141307993866256225615783033603165251855960",10)
+	pt:= New_point(*u,*d)
+	fmt.Println(pt.val)
+	fmt.Println(pt.Sign())
+	pt2:=Raw_point()
+	copy(pt2.val,pt.val)
+	sum:=pt.Add(pt2)
+	fmt.Println(sum)
+	diff:= sum.Sub(pt2)
+	fmt.Println(diff)
+	two:= big.NewInt(2)
+	Scalar_2:= New_scalar(*two)
+	fmt.Println(pt.Mul(Scalar_2))
+	fmt.Println(Random().val)
+	// fmt.Println(C.crypto_core_ed25519_Is_valid_point((*C.uchar)(&pt.val[0])))
+	fmt.Println(pt.Is_valid())
+	s := New_scalar(*big.NewInt(2))
+	// e := New_scalar(*big.NewInt(3))
+	t,_:=s.Inverse()
+	fmt.Println(t.Mul(s))
+	token := make([]byte,32)
 
-// }
+	// fmt.Println(C.crypto_scalarmult_ed25519_base_noclamp((*C.uchar)(&token[0]),(*C.uchar)(&s.val[0])))
+	C.crypto_scalarmult_ed25519_noclamp((*C.uchar)(&token[0]),(*C.uchar)(&s.val[0]),(*C.uchar)(&pt.val[0]));
+	fmt.Println(token)
+	fmt.Println(C.crypto_core_ed25519_is_valid_point((*C.uchar)(&token[0])))
+    
+}
--- crypto/ed25519/helper.h
+++ crypto/ed25519/helper.h
@@ -1,9 +1,4 @@
-/*
- * person.h
- * Copyright (C) 2019 Tim Hughes
- *
- * Distributed under terms of the MIT license.
- */
+
 
 #ifndef HELPER_H
 #define HELPER_H
@@ -24,5 +19,23 @@ int crypto_scalarmult_ed25519_base_noclamp(unsigned char *q, const unsigned char
 int crypto_scalarmult_ed25519_noclamp(unsigned char *q, const unsigned char *n,
                                       const unsigned char *p);
 int sodium_memcmp(const void * const b1_, const void * const b2_, size_t len);
+void crypto_core_ed25519_scalar_reduce(unsigned char *r, const unsigned char *s);
+void crypto_core_ed25519_scalar_add(unsigned char *z,
+                                    const unsigned char *x, const unsigned char *y);
+void crypto_core_ed25519_scalar_sub(unsigned char *z,
+                                    const unsigned char *x, const unsigned char *y);
+void crypto_core_ed25519_scalar_mul(unsigned char *z,
+                                    const unsigned char *x, const unsigned char *y);
+void crypto_core_ed25519_scalar_negate(unsigned char *neg, const unsigned char *s);
+int crypto_core_ed25519_scalar_invert(unsigned char *recip, const unsigned char *s);
+int crypto_sign_seed_keypair(unsigned char *pk, unsigned char *sk,
+                             const unsigned char *seed);
+int crypto_sign_detached(unsigned char *sig, unsigned long long *siglen_p,
+                         const unsigned char *m, unsigned long long mlen,
+                         const unsigned char *sk);
+int crypto_sign_verify_detached(const unsigned char *sig,
+                                const unsigned char *m,
+                                unsigned long long mlen,
+                                const unsigned char *pk);
 
 #endif /* !HELPER_H */
